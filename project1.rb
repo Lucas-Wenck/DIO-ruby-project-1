31b4d5532560626d61cbbe99a8036543907fe3b2
Lucas Wenck <lucas.wenck@gmail.com>
2023-03-14 23:19:07 -0300

    Finish the code of the project

---
+++ project1.rb
@@ -0,0 +1,8 @@
+puts "Qual o seu nome?"
+nome = gets.chomp
+puts "Qual o seu sobrenome?"
+sobrenome = gets.chomp
+puts "E por ultimo qual a sua idade?"
+idade = gets.chomp
+
+puts "Seu nome completo é #{nome} #{sobrenome} e a sua idade é #{idade} anos"
